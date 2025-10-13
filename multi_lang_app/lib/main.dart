d9cf45b981c9b48918c47d8913d5f45cf204c337
kihyun <houng027@gmail.com>
2025-10-13 13:48:58 +0900

    chore: 앱 타이틀 변경 'Multilingual Translation Synchronization Project'

--- multi_lang_app/lib/main.dart
+++ multi_lang_app/lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hearing Survey',
+      title: 'Multilingual Translation Synchronization Project',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const HomePage(),
     );
